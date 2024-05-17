d939307a070fb56dd9dbad5c6124a207676469c6
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2024-05-17 08:53:00 -0300

    Initial Commit

---
+++ MultiplatformApp/MultiplatformAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplatformAppApp.swift
+//  MultiplatformApp
+//
+//  Created by Bruno Oliveira on 17/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MultiplatformAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
